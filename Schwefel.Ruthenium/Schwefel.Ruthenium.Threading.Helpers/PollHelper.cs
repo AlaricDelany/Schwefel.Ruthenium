2f00bd95e24ae3623121265b9e8d60dffb4c94c6
Alaric Delany <alaric-delany@live.de>
2016-11-18 17:38:59 +0100

    Schwefel.Ruthenium.Threading.Helpers - Initial Commit - Added PollHelper

---
+++ Schwefel.Ruthenium/Schwefel.Ruthenium.Threading.Helpers/PollHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Schwefel.Ruthenium.Threading.Helpers
+{
+    public static class PollHelper
+    {
+        public class PollHelperResult
+        {
+            public PollHelperResult()
+            {
+                Exception = null;
+                IsFinished = false;
+            }
+
+            public bool IsFinished { get; set; }
+
+            public bool IsSucceded => Exception == null;
+
+            public Exception Exception { get; set; }
+        }
+
+        public static async Task<PollHelperResult> PollUntilTrueAsync(Func<bool> pollFunc, TimeSpan timeSpanTillTimeout,
+            string timeoutErrorMessage, TimeSpan? pollInterval = null)
+        {
+            if(pollInterval == null || pollInterval < TimeSpan.Zero)
+                pollInterval = TimeSpan.Zero;
+
+            DateTime lStartTime = DateTime.UtcNow;
+            PollHelperResult lResult = new PollHelperResult();
+
+            while(!lResult.IsFinished)
+            {
+                if((lStartTime + timeSpanTillTimeout) >= DateTime.UtcNow)
+                    throw new TimeoutException(timeoutErrorMessage);
+
+                DateTime lStartExecution = DateTime.UtcNow;
+                DateTime lEndExecution = lStartExecution;
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        lResult.IsFinished = pollFunc();
+                    }
+                    catch(Exception ex)
+                    {
+                        lResult.Exception = ex;
+                        lResult.IsFinished = true;
+                    }
+                    lEndExecution = DateTime.UtcNow;
+
+                });
+                TimeSpan lTimeToWaitTillNextInterval = pollInterval.Value - (lEndExecution - lStartExecution);
+
+                await Task.Delay(lTimeToWaitTillNextInterval);
+            }
+            return lResult;
+
+        }
+
+        public static PollHelperResult PollUntilTrue(Func<bool> pollFunc, TimeSpan timeSpanTillTimeout,
+            string timeoutErrorMessage, TimeSpan? pollInterval = null)
+        {
+            Task<PollHelperResult> lPollTask = PollUntilTrueAsync(pollFunc, timeSpanTillTimeout, timeoutErrorMessage,
+                pollInterval);
+
+            lPollTask.ConfigureAwait(false);
+
+            return lPollTask.Result;
+        }
+    }
+}
---
+++ Schwefel.Ruthenium/Schwefel.Ruthenium.Threading.Helpers/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Schwefel.Ruthenium.Threading.Helpers")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0272a212-41f8-42f6-ae23-f49728e46e45")]
