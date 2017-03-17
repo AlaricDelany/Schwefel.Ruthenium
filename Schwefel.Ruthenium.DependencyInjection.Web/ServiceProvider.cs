caa875886a9e801d46f63143b3fcea90d87c68c4
Alaric Delany <alaric-delany@live.de>
2017-03-17 14:46:14 +0100

    Added Schwefel.Ruthenium.DependencyInjection.Web

---
+++ Schwefel.Ruthenium.DependencyInjection.Web/ServiceProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Schwefel.Ruthenium.DependencyInjection.Web
+{
+    public class ServiceProvider<TDependencyContainer> : IServiceProvider, IDisposable
+        where TDependencyContainer : IDependencyInjectionContainer
+    {
+        private IDependencyInjectionContainer _container;
+
+        public ServiceProvider(IDependencyInjectionContainer container)
+        {
+            _container = container;
+        }
+
+        public void Dispose()
+        {
+            _container?.Dispose();
+            _container = null;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _container.Resolve(serviceType);
+        }
+    }
+}
