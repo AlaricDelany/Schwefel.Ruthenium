e82e0842aa50853935bf35c4ae872eb4b70a8100
Alaric Delany <alaric-delany@live.de>
2017-10-15 23:06:46 +0200

    Start Using Paket as Package Manager

--- Schwefel.Ruthenium.DependencyInjection.UnityAdapterNet45/Modules/IUnityModule.cs
+++
@@ -1,9 +0,0 @@
-﻿using Schwefel.Ruthenium.DependencyInjection.Modules;
-
-namespace Schwefel.Ruthenium.DependencyInjection.UnityAdapterNet45.Modules
-{
-    public interface IUnityModule : IModule
-    {
-        void Configure(UnityDependencyInjectionContainer container);
-    }
-}
